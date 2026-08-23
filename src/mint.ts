bfc4353bc45d9b2e12015466d23fdc77bace6024
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-08-23 17:51:22 +0200

    Update mint.ts

--- src/mint.ts
+++ src/mint.ts
@@ -1,5 +1,6 @@
 import wallet from './wallet'
 import { signAndSubmitTx } from './txHelpers'
+import { counterValidator, mintPolicyScript } from './loadValidator'
 
 function strToHex(s: string) {
   const encoder = new TextEncoder()
@@ -13,6 +14,12 @@ function strToHex(s: string) {
 // - policyId: hex currency symbol of the minting policy (published on-chain)
 // - saleAddress: address that should receive the price for the mint
 // - priceLovelace: price to pay (in lovelace)
+//
+// IMPORTANT: `policyId` must be the currency symbol that results from
+// applying MintPolicy with the SAME saleAddress/priceLovelace parameters
+// that were used when running `cabal run export-scripts` to produce
+// mintPolicy.plutus.json. If they don't match, the on-chain currency symbol
+// won't match what this function mints under, and the tx will fail.
 export async function mintSerialNFT(counterScriptAddress: string, policyId: string, saleAddress: string, priceLovelace = 2000000) {
   const lucid = wallet.getLucid()
   if (!lucid) throw new Error('Wallet not connected')
@@ -26,7 +33,6 @@ export async function mintSerialNFT(counterScriptAddress: string, policyId: stri
   let n: number | null = null
   try {
     const datum = counterUtxo.datum || (await lucid.datumOf(counterUtxo))
-    // If datum is a plain integer represented as number or BigInt
     if (typeof datum === 'number') n = datum
     else if (typeof datum === 'bigint') n = Number(datum)
     else if (datum && typeof datum === 'object' && ('int' in datum)) n = Number((datum as any).int)
@@ -44,20 +50,21 @@ export async function mintSerialNFT(counterScriptAddress: string, policyId: stri
   mintValue[policyId] = {}
   mintValue[policyId][tokenNameHex] = 1
 
-  // Build the tx:
-  // - consume counter UTxO
-  // - mint the single NFT named by the counter
-  // - pay price to saleAddress
-  // - create new counter UTxO at same script with datum n+1 (we place minimal ADA)
-
   const minAdaForCounter = 2000000n
   const newCounterDatum = n + 1
 
+  // Build the tx:
+  // - consume counter UTxO -> requires CounterValidator attached (was missing)
+  // - mint the single NFT named by the counter -> requires MintPolicy
+  //   attached as minting policy (was missing)
+  // - pay price to saleAddress
+  // - recreate counter UTxO at same script with datum n+1
   const tx = await lucid.newTx()
     .collectFrom([counterUtxo], lucid.Data.void())
-    .mintAssets(mintValue)
+    .attachSpendingValidator(counterValidator)
+    .mintAssets(mintValue, lucid.Data.void())
+    .attachMintingPolicy(mintPolicyScript)
     .payToAddress(saleAddress, { lovelace: BigInt(priceLovelace) })
-    // Recreate counter UTxO: pay to script address with inline datum
     .payToContract(counterScriptAddress, { inline: lucid.Data.to(newCounterDatum) }, { lovelace: minAdaForCounter })
     .addSigner(await lucid.wallet.address())
     .complete()
