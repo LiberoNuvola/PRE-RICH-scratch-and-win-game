b2cef69e9775d6cf61634595103cb44a1c4950c7
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-08-23 13:55:43 +0200

    Update PrizeValidator.hs

--- plutus/PrizeValidator.hs
+++ plutus/PrizeValidator.hs
@@ -1,113 +1,141 @@
-
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module PrizeValidator where
 
--- Minimal Plutus V2 validator skeleton for prize UTxO.
--- Datum: information about prize (assetId, amount, ticket policy etc).
--- Redeemer: claim attempt. Validator checks that the claimant presents (consumes) the corresponding ticket token
--- and that the tx is signed by the claimant address.
+-- Validator del prize UTxO.
+-- Claim valido solo se:
+--   1) firmato dal claimant
+--   2) esattamente un input di questo script nella tx (un claim per tx)
+--   3) il prize UTxO contiene almeno l'importo dichiarato nel datum
+--   4) il ticket e' negli input
+--   5) il ticket viene bruciato (mint == -1)
+--   6) il premio viene pagato al claimant
 
-import           Plutus.V2.Ledger.Api      as PlutusV2
-import           Plutus.V2.Ledger.Contexts as Contexts
-import qualified Plutus.V1.Ledger.Value    as V
-import           Plutus.V1.Ledger.Value    (Value)
-import           Plutus.V1.Ledger.Ada      as Ada
-import           Plutus.V1.Ledger.Credential (Credential(..))
-import           PlutusTx.Prelude         hiding (Semigroup(..), unless)
-import qualified PlutusTx
-import           Prelude                  (String)
+import           Plutus.V2.Ledger.Api
+import           Plutus.V2.Ledger.Contexts
+import           PlutusTx
+import           PlutusTx.Prelude         hiding (Semigroup (..), unless)
 
 data PrizeDatum = PrizeDatum
-    { pdAssetId         :: BuiltinByteString
-    , pdPrizeAmount     :: Integer
-    , pdTicketPolicy    :: BuiltinByteString
-    , pdTicketName      :: BuiltinByteString
-    , pdPaymentPolicy   :: BuiltinByteString
-    , pdPaymentName     :: BuiltinByteString
-    , pdClaimantPkh     :: PubKeyHash
-    }
+  { pdPrizeAmount   :: Integer
+  , pdTicketPolicy  :: BuiltinByteString
+  , pdTicketName    :: BuiltinByteString
+  , pdPaymentPolicy :: BuiltinByteString -- empty = ADA
+  , pdPaymentName   :: BuiltinByteString
+  , pdClaimantPkh   :: PubKeyHash
+  }
 
 PlutusTx.unstableMakeIsData ''PrizeDatum
+PlutusTx.makeLift ''PrizeDatum
 
 {-# INLINABLE claimAddress #-}
 claimAddress :: PubKeyHash -> Address
 claimAddress pkh = Address (PubKeyCredential pkh) Nothing
 
+{-# INLINABLE countOwnScriptInputs #-}
+countOwnScriptInputs :: ScriptContext -> Integer
+countOwnScriptInputs ctx =
+  let thisScriptHash = ownHash ctx  -- rinominato per non fare shadowing di
+                                     -- Contexts.ownHash (che altrimenti
+                                     -- diventerebbe auto-ricorsivo e non
+                                     -- tiperebbe)
+      info = scriptContextTxInfo ctx
+  in  foldr
+        (\txIn acc ->
+            case addressCredential (txOutAddress (txInInfoResolved txIn)) of
+              ScriptCredential vh
+                | vh == thisScriptHash -> acc + 1
+              _ -> acc
+        )
+        0
+        (txInfoInputs info)
+
 {-# INLINABLE mkValidator #-}
-mkValidator :: PrizeDatum -> BuiltinData -> ScriptContext -> Bool
+mkValidator :: PrizeDatum -> () -> ScriptContext -> Bool
 mkValidator datum _ ctx =
-    let
-        info :: TxInfo
-        info = scriptContextTxInfo ctx
+  let info = scriptContextTxInfo ctx
 
-        claimantPkh :: PubKeyHash
-        claimantPkh = pdClaimantPkh datum
+      claimantPkh = pdClaimantPkh datum
+      signedOK    = claimantPkh `elem` txInfoSignatories info
 
-        signatories :: [PubKeyHash]
-        signatories = txInfoSignatories info
+      -- Un solo prize claim per transazione: impedisce di aggregare piu'
+      -- prize UTxO di questo stesso script nella stessa tx e far coprire
+      -- a un unico output condiviso piu' premi contemporaneamente.
+      singleOwnInput = countOwnScriptInputs ctx == 1
 
-        signedOK :: Bool
-        signedOK = elem claimantPkh signatories
+      ticketCs = CurrencySymbol (pdTicketPolicy datum)
+      ticketTn = TokenName (pdTicketName datum)
 
-        containsTicket :: Value -> Bool
-        containsTicket val = let
-            cs = CurrencySymbol (pdTicketPolicy datum)
-            tn = TokenName (pdTicketName datum)
-            amt = V.valueOf val cs tn
-          in amt > 0
+      containsTicket val = valueOf val ticketCs ticketTn > 0
 
-        hasTicket :: Bool
-        hasTicket = any (\txInInfo -> containsTicket (txOutValue $ txInInfoResolved txInInfo)) (txInfoInputs info)
+      hasTicket =
+        any
+          (\txIn -> containsTicket (txOutValue (txInInfoResolved txIn)))
+          (txInfoInputs info)
 
-        -- Require the ticket to be burned in this same transaction so it can
-        -- never be reused to claim a second prize UTxO with the same policy/name.
-        ticketBurned :: Bool
-        ticketBurned = let
-            cs = CurrencySymbol (pdTicketPolicy datum)
-            tn = TokenName (pdTicketName datum)
-          in V.valueOf (txInfoMint info) cs tn == negate 1
+      ticketBurned =
+        valueOf (txInfoMint info) ticketCs ticketTn == negate 1
 
-        claimantAddress :: Address
-        claimantAddress = claimAddress claimantPkh
+      isAdaPayment = lengthOfByteString (pdPaymentPolicy datum) == 0
 
-        sumAdaToClaimant :: Integer
-        sumAdaToClaimant = foldr (\txOut acc ->
-                if txOutAddress txOut == claimantAddress
-                    then acc + V.valueOf (txOutValue txOut) Ada.adaSymbol Ada.adaToken
+      -- Il prize UTxO deve contenere davvero il premio dichiarato nel datum,
+      -- non solo dichiararlo a parole.
+      ownValueCoversPrize =
+        case findOwnInput ctx of
+          Nothing -> False
+          Just i  ->
+            let v = txOutValue (txInInfoResolved i)
+            in  if isAdaPayment
+                  then valueOf v adaSymbol adaToken >= pdPrizeAmount datum
+                  else
+                    let payCs = CurrencySymbol (pdPaymentPolicy datum)
+                        payTn = TokenName (pdPaymentName datum)
+                    in  valueOf v payCs payTn >= pdPrizeAmount datum
+
+      claimantAddr = claimAddress claimantPkh
+
+      sumAdaToClaimant =
+        foldr
+          (\txOut acc ->
+              if txOutAddress txOut == claimantAddr
+                then acc + valueOf (txOutValue txOut) adaSymbol adaToken
+                else acc
+          )
+          0
+          (txInfoOutputs info)
+
+      sumAssetToClaimant =
+        let payCs = CurrencySymbol (pdPaymentPolicy datum)
+            payTn = TokenName (pdPaymentName datum)
+        in  foldr
+              (\txOut acc ->
+                  if txOutAddress txOut == claimantAddr
+                    then acc + valueOf (txOutValue txOut) payCs payTn
                     else acc
-            ) 0 (txInfoOutputs info)
+              )
+              0
+              (txInfoOutputs info)
 
-        sumAssetToClaimant :: Integer
-        sumAssetToClaimant = let
-                cs = CurrencySymbol (pdPaymentPolicy datum)
-                tn = TokenName (pdPaymentName datum)
-            in foldr (\txOut acc ->
-                    if txOutAddress txOut == claimantAddress
-                        then acc + V.valueOf (txOutValue txOut) cs tn
-                        else acc
-                ) 0 (txInfoOutputs info)
+      paysPrize =
+        if isAdaPayment
+          then sumAdaToClaimant >= pdPrizeAmount datum
+          else sumAssetToClaimant >= pdPrizeAmount datum
 
-        isAdaPayment :: Bool
-        isAdaPayment = lengthOfByteString (pdPaymentPolicy datum) == 0
-
-        paysPrize :: Bool
-        paysPrize = if isAdaPayment
-            then sumAdaToClaimant >= pdPrizeAmount datum
-            else sumAssetToClaimant >= pdPrizeAmount datum
-
-    in
-        traceIfFalse "Missing claimant signature" signedOK &&
-        traceIfFalse "Ticket not provided in inputs" hasTicket &&
-        traceIfFalse "Ticket must be burned in the claim tx" ticketBurned &&
-        traceIfFalse "Prize not paid to claimant" paysPrize
+  in  traceIfFalse "Missing claimant signature" signedOK
+        && traceIfFalse "Only one prize claim allowed per transaction" singleOwnInput
+        && traceIfFalse "Prize UTxO does not cover declared amount" ownValueCoversPrize
+        && traceIfFalse "Ticket not provided in inputs" hasTicket
+        && traceIfFalse "Ticket must be burned in the claim tx" ticketBurned
+        && traceIfFalse "Prize not paid to claimant" paysPrize
 
 validator :: Validator
-validator = mkValidatorScript ($$(PlutusTx.compile [|| mkValidator ||]))
+validator =
+  mkValidatorScript
+    $$(compile [|| mkUntypedValidator mkValidator ||])
 
 valHash :: ValidatorHash
 valHash = validatorHash validator
