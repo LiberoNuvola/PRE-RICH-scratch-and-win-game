2d82a6300816ee9a547d679f46efa18a0cac045c
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-08-23 14:00:34 +0200

    Update CounterValidator.hs

--- plutus/CounterValidator.hs
+++ plutus/CounterValidator.hs
@@ -1,57 +1,86 @@
+```haskell
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module CounterValidator where
 
-import           Plutus.V2.Ledger.Api      as PlutusV2
-import           PlutusTx.Prelude         hiding (Semigroup(..), unless)
-import qualified PlutusTx
-import           Prelude                  (String)
+import           Plutus.V2.Ledger.Api
+import           Plutus.V2.Ledger.Contexts
+import           PlutusTx
+import           PlutusTx.Prelude         hiding (Semigroup (..), unless)
 
--- The counter UTxO is the serialisation source for minted ticket NFTs.
--- It is intentionally simple: the datum is the current counter value, and any
--- valid spend must produce the next datum value at the same script address.
-newtype CounterDatum = CounterDatum Integer
-  deriving (Show)
+-- Datum = contatore corrente (Integer puro, compatibile con lucid Data.to(n)).
+-- Spend valido solo se:
+--   1) esiste esattamente un output allo stesso script con datum n+1
+--   2) i lovelace bloccati sullo script non diminuiscono
 
-PlutusTx.unstableMakeIsData ''CounterDatum
-PlutusTx.makeLift ''CounterDatum
+{-# INLINABLE readIntegerDatum #-}
+readIntegerDatum :: TxInfo -> TxOut -> Maybe Integer
+readIntegerDatum info out =
+  case txOutDatum out of
+    OutputDatum d ->
+      case fromBuiltinData (getDatum d) of
+        Just (n :: Integer) -> Just n
+        Nothing             -> Nothing
+    OutputDatumHash dh ->
+      case findDatum dh info of
+        Just d ->
+          case fromBuiltinData (getDatum d) of
+            Just (n :: Integer) -> Just n
+            Nothing             -> Nothing
+        Nothing -> Nothing
+    NoOutputDatum -> Nothing
+
+{-# INLINABLE lovelaceOf #-}
+lovelaceOf :: Value -> Integer
+lovelaceOf v = valueOf v adaSymbol adaToken
 
 {-# INLINABLE mkValidator #-}
-mkValidator :: CounterDatum -> BuiltinData -> ScriptContext -> Bool
-mkValidator (CounterDatum current) _ ctx =
+mkValidator :: Integer -> () -> ScriptContext -> Bool
+mkValidator current _ ctx =
   let info = scriptContextTxInfo ctx
-      ownInput = findOwnInput ctx
-      ownAddress = case ownInput of
-        Just i  -> txOutAddress (txInInfoResolved i)
-        Nothing -> traceError "CounterValidator: missing own input"
-      outputs = txInfoOutputs info
-      hasNextCounter =
-        any
-          (\o ->
-            txOutAddress o == ownAddress &&
-            case txOutDatumHash o of
-              Just dh ->
-                case findDatum dh info of
-                  Just d ->
-                    case PlutusTx.fromBuiltinData (getDatum d) of
-                      Just (CounterDatum next) -> next == current + 1
-                      _ -> False
-                  Nothing -> False
-              Nothing -> False
-          )
-          outputs
-  in traceIfFalse "CounterValidator: missing own input" (isJust ownInput) &&
-     traceIfFalse "CounterValidator: next counter datum missing" hasNextCounter
+  in case findOwnInput ctx of
+       Nothing -> traceError "CounterValidator: missing own input"
+       Just i  ->
+         let ownAddress    = txOutAddress (txInInfoResolved i)
+             ownInLovelace = lovelaceOf (txOutValue (txInInfoResolved i))
+
+             scriptOuts =
+               filter (\o -> txOutAddress o == ownAddress) (txInfoOutputs info)
+
+             nextCounterOuts =
+               filter
+                 (\o ->
+                     case readIntegerDatum info o of
+                       Just next -> next == current + 1
+                       Nothing   -> False
+                 )
+                 scriptOuts
+
+             exactlyOneNext = length nextCounterOuts == 1
+             noExtraScriptOuts = length scriptOuts == 1
+
+             valuePreserved =
+               case nextCounterOuts of
+                 [o] -> lovelaceOf (txOutValue o) >= ownInLovelace
+                 _   -> False
+
+         in  traceIfFalse "CounterValidator: expected exactly one n+1 counter output" exactlyOneNext
+               && traceIfFalse "CounterValidator: unexpected extra script outputs" noExtraScriptOuts
+               && traceIfFalse "CounterValidator: script value was drained" valuePreserved
 
 validator :: Validator
-validator = mkValidatorScript ($$(PlutusTx.compile [|| mkValidator ||]))
+validator =
+  mkValidatorScript
+    $$(compile [|| mkUntypedValidator mkValidator ||])
 
 counterValidatorHash :: ValidatorHash
-counterValidatorHash = PlutusV2.validatorHash validator
-
+counterValidatorHash = validatorHash validator
 validatorScript :: Script
 validatorScript = unValidatorScript validator
+```
+
+migliorati ulteriormente, credo
